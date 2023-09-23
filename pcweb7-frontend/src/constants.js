72c6ea37553d9d710199e6109afcb2532299c0e3
ElizabethFoo <elizabethfoo_93@hotmail.com>
2023-09-23 14:28:00 +0800

    Modified API URL

--- pcweb7-frontend/src/constants.js
+++ pcweb7-frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:3000"; // https://pcweb7-final-project-worK_Break.vercel.app/api
+export const API = "https://pcweb7-final-project.vercel.app/api", // http://localhost:3000"
 export const LEAVES = "/leaves";
 export const ADD = "/add";
 export const POST = "/post";
